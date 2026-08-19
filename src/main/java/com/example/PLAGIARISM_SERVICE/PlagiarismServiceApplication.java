4fc47c730ee8c5917077dd173410db5ee1ab03f7
Gideon0123 <nwokporochijiokegideon@gmail.com>
2026-08-19 09:33:38 +0100

    implementations

--- src/main/java/com/example/PLAGIARISM_SERVICE/PlagiarismServiceApplication.java
+++ src/main/java/com/example/PLAGIARISM_SERVICE/PlagiarismServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.PLAGIARISM_SERVICE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class PlagiarismServiceApplication {
 
 	public static void main(String[] args) {
