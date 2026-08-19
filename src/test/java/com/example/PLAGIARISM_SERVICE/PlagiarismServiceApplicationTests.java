c12ac21eea8d84403ed0bbf296d6396b3ac54b7f
Gideon0123 <nwokporochijiokegideon@gmail.com>
2026-08-19 03:47:09 +0100

    first commit

---
+++ src/test/java/com/example/PLAGIARISM_SERVICE/PlagiarismServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PLAGIARISM_SERVICE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlagiarismServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
